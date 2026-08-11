d7cf4b86940b82cba07b534227f37c38b6a32472
benevanoff <benjaminevanoff99@gmail.com>
2026-08-10 20:35:30 -0500

    frontend: and navbar login click handler

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -39,7 +39,7 @@ const NavTab = ({ isOpen }) => {
         <div className={`navbar ${isOpen ? 'open' : ''}`}>
             {isVendor && <button onClick={() => {navigate('/create_listing')}}>Create Listing</button>}
             {isVendor && <button onClick={() => {navigate('/vendor/orders')}}>Vendor Orders</button>}
-            {userDetails ? <button>Logout</button> : <button>Login</button>}
+            {userDetails ? <button>Logout</button> : <button onClick={() => {navigate('/login')}}>Login</button>}
         </div>
     );
 };
